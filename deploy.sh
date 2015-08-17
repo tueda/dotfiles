94a8fe6ce980500446543dbff95312139eb4490e
Takahiro Ueda <tueda@nikhef.nl>
2015-08-17 19:53:09 +0200

    deploy.sh: Do git submodule update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #! /bin/sh
 set -e
+cd `dirname $0`
 
 backupdir=backup/`date +'%Y%m%d%H%M%S'`
 
-cd `dirname $0`
+if [ -f .gitmodules ]; then
+  if type git >/dev/null 2>&1; then
+    git submodule update --init
+  else
+    echo "info: initializing/updating submodules requires git"
+  fi
+fi
 
 list_dotfiles() {
   for file in .*; do
@@ -18,8 +25,9 @@ list_dotfiles() {
 backup=false
 
 for file in `list_dotfiles`; do
-  if [ -e "$HOME/$file" ]; then
-    if [ -L "$HOME/$file" ] && [ `readlink "$HOME/$file"` == `pwd`/"$file" ]; then
+  if [ -e "$HOME/$file" ] || [ -h "$HOME/$file" ]; then
+#   readlink is not in POSIX, but expected to work both on Linux and OS X.
+    if [ -h "$HOME/$file" ] && [ `readlink "$HOME/$file"` = `pwd`/"$file" ]; then
       echo "skip: $file exists"
       continue
     fi
